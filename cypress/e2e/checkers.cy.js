26e95cc3152ee9c4653c9766034a448ca556a0bc
Kim Bell <krbell4@gmail.com>
2024-08-18 17:43:06 -0400

    add final tests and dry up, runs 12 seconds

--- cypress/e2e/checkers.cy.js
+++ cypress/e2e/checkers.cy.js
@@ -15,6 +15,14 @@ describe("Checkers Game UI", () => {
 		cy.get('#message').should('have.text', text).and(visible)
 	}
 
+	const yourFirstMove = (selectedPiece, moveTo) => {
+		cy.get(`img[name=${selectedPiece}]`).should('have.attr','src','you1.gif').click()
+		// After an orange piece is selected the image changes
+		cy.get(`img[name=${selectedPiece}]`).should('have.attr','src','you2.gif')
+		// Select a grey space to move the piece to
+		cy.get(`img[name=${moveTo}]`).should('have.attr','src','gray.gif').click()
+	}
+
   it("Should have header, game content, navigation, and footer", () => {
 		const rulesHref = "https://en.wikipedia.org/wiki/English_draughts#Starting_position"
 	// // cy.window().then(win => {
@@ -63,22 +71,14 @@ describe("Checkers Game UI", () => {
 
 	it('Should display message "Make a move."', () => {
 		cy.get(".content").within(() => {
-			cy.get('img[name="space42"]').should('have.attr','src','you1.gif').click()
-			// After an orange piece is selected the image changes
-			cy.get('img[name="space42"]').should('have.attr','src','you2.gif')
-			// Select a grey space to move the piece to
-			cy.get('img[name="space33"]').should('have.attr','src','gray.gif').click()
+			yourFirstMove("space42", "space33")
 			verifyMessage("Make a move.", "be.visible")
 		})
 	})
 
 	it('Should display message "Move diagonally only."', () => {
 		cy.get(".content").within(() => {
-			cy.get('img[name="space42"]').should('have.attr','src','you1.gif').click()
-			// After an orange piece is selected the image changes
-			cy.get('img[name="space42"]').should('have.attr','src','you2.gif')
-			// Select a grey space to move the piece to
-			cy.get('img[name="space33"]').should('have.attr','src','gray.gif').click()
+			yourFirstMove("space42", "space33")
 			// Make sure it is your turn to move again
 			verifyMessage("Make a move.", "be.visible")
 			cy.wait(1000) // bad practice but no other way
@@ -88,9 +88,27 @@ describe("Checkers Game UI", () => {
 			verifyMessage("Move diagonally only.", "be.visible")
 		})
 	})
-  // Messages:
+	it('Should display message "Please wait."', () => {
+		cy.get(".content").within(() => {
+			yourFirstMove("space42", "space33")
+			// Make sure it is your turn to move again
+			verifyMessage("Make a move.", "be.visible")
+			cy.get('img[name="space33"]').should('have.attr','src','you1.gif').click()
+			// Select a grey space horizontal to your location to move the piece to
+			cy.get('img[name="space53"]').click()
+			verifyMessage("Please wait.", "be.visible")
+		})
+	})
 
-  // please wait
-
-  // “This is an invalid move” - trying to  jump a valid blue piece on a square not touching your piece
+	it('Should display message "This is an invalid move."', () => {
+		cy.get(".content").within(() => {
+			yourFirstMove("space42", "space53")
+			// Make sure it is your turn to move again
+			verifyMessage("Make a move.", "be.visible")
+			cy.wait(1000) // bad practice but no other way
+			cy.get('img[name="space51"]').should('have.attr','src','you1.gif').click()
+			cy.get('img[name="space15"]').click()
+			verifyMessage("This is an invalid move.", "be.visible")
+		})
+	})
 })
